74c93b6d1bacdbc2f78a95f0f4502b2df0fe6ab1
Arturo <caramono@gmail.com>
2016-07-29 13:50:48 +0200

    retoques, GPRS funcionando

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 12 */
+/* This file is auto generated, version 13 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#12 SMP PREEMPT Thu Jul 28 15:39:19 CEST 2016"
+#define UTS_VERSION "#13 SMP PREEMPT Fri Jul 29 13:36:51 CEST 2016"
 #define LINUX_COMPILE_BY "arturo"
 #define LINUX_COMPILE_HOST "Asapina"
 #define LINUX_COMPILER "gcc version 5.4.0 20160609 (Ubuntu/Linaro 5.4.0-6ubuntu1~16.04.1) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,2 +1,2 @@
-#define UTS_RELEASE "3.8.13.30clickarm-g10784cd-dirty"
-#define KERNEL_GIT_ID "10784cd"
+#define UTS_RELEASE "3.8.13.30clickarm-geb9cd19-dirty"
+#define KERNEL_GIT_ID "eb9cd19"
