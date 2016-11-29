e4c33fbefb43f165718ff915bb587990be02ef45
Eric Bosch <ebosch@twonav.com>
2016-11-29 14:22:45 +0100

    First approach

---
+++ debian/linux-headers-3.8.13.30-gcbd585a-dirty/usr/src/linux-headers-3.8.13.30-gcbd585a-dirty/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 SMP PREEMPT Tue Nov 29 13:57:17 CET 2016"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "ebosch-H81M-S2H"
+#define LINUX_COMPILER "gcc version 4.8.4 (Ubuntu/Linaro 4.8.4-2ubuntu1~14.04.1) "
---
+++ debian/linux-headers-3.8.13.30-gcbd585a-dirty/usr/src/linux-headers-3.8.13.30-gcbd585a-dirty/include/generated/utsrelease.h
@@ -0,0 +1,2 @@
+#define UTS_RELEASE "3.8.13.30-gcbd585a-dirty"
+#define KERNEL_GIT_ID "cbd585a"
