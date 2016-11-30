db0c05801209606e8f786f6bfbcf65a2f8118a3f
Eric Bosch <ebosch@twonav.com>
2016-11-30 16:36:53 +0100

    cleaning up compilation

--- debian/linux-headers-3.8.13.30-twonav-g123800b-dirty/usr/src/linux-headers-3.8.13.30-twonav-g123800b-dirty/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 2 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#2 SMP PREEMPT Wed Nov 30 16:27:33 CET 2016"
-#define LINUX_COMPILE_BY "root"
-#define LINUX_COMPILE_HOST "ebosch-H81M-S2H"
-#define LINUX_COMPILER "gcc version 4.8.4 (Ubuntu/Linaro 4.8.4-2ubuntu1~14.04.1) "
--- debian/linux-headers-3.8.13.30-twonav-g123800b-dirty/usr/src/linux-headers-3.8.13.30-twonav-g123800b-dirty/include/generated/utsrelease.h
+++
@@ -1,2 +0,0 @@
-#define UTS_RELEASE "3.8.13.30-twonav-g123800b-dirty"
-#define KERNEL_GIT_ID "123800b"
