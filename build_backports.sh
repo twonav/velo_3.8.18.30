8a04786769caa32a4314fb61b584df9b2804bd90
dnieto <dnieto@twonav.com>
2016-12-09 16:53:26 +0100

    Generic compile Backports

--- build_backports.sh
+++ build_backports.sh
@@ -1,16 +1,42 @@
+# http://confluence.compegps.com/display/TV/Cross+Compiling+Wireless+Backports
+
+# Requirements:
+# arm-linux-gnueabihf-gcc
+
+if [[ ! $(whoami) =~ "root" ]]; then
+echo ""
+echo "**********************************"
+echo "*** This should be run as root ***"
+echo "**********************************"
+echo ""
+exit
+fi
+
+if [[ -z $1 ]]; then
+echo "Usage: ./build_backports.sh <USER>"
+echo " <USER> is the name of home subfolder (ebosch, dnieto, etc)"
+exit
+fi
+
+HOMEUSERFOLDER=$1
+
 #1.EXPORT REQUIRED VARIABLES
 
 export CCPREFIX=/opt/toolchains/gcc-linaro-arm-linux-gnueabihf-4.7-2013.04-20130415_linux/bin/arm-linux-gnueabihf-
-export KERNEL_SRC=/home/ebosch/Kernels_IMASD/Clickarm_Kernel_3.8
+export KERNEL_SRC=/home/$HOMEUSERFOLDER/Kernels_IMASD/Clickarm_Kernel_3.8
 export ARCH=arm
 export CROSS_COMPILE=/opt/toolchains/gcc-linaro-arm-linux-gnueabihf-4.7-2013.04-20130415_linux/bin/arm-linux-gnueabihf-
-export BACK_PORTS=/home/ebosch/Kernels_IMASD/backports-3.17.1-1
-export KLIB_BUILD=/home/ebosch/Kernels_IMASD/Clickarm_Kernel_3.8
-export KLIB=/home/ebosch/Velo_images/kernel_modules
+export BACK_PORTS=/home/$HOMEUSERFOLDER/Kernels_IMASD/backports-3.17.1-1
+export KLIB_BUILD=/home/$HOMEUSERFOLDER/Kernels_IMASD/Clickarm_Kernel_3.8
+export KLIB=/home/$HOMEUSERFOLDER/Velo_images/kernel_modules
+
+echo "**** STEP 1 END EXPORT REQUIRED VARIABLES ****"
 
 #2.CLEAN PREVIOUS COMPILATION
 rm -rf $KLIB/lib/modules/*
-rm -rf /media/ebosch/trusty/lib/modules/*
+rm -rf /media/$HOMEUSERFOLDER/trusty/lib/modules/*
+
+echo "**** STEP 2 END CLEAN PREVIOUS COMPILATION ****"
 
 #3.BUILD KERNEL AS USUAL
 
@@ -21,7 +47,9 @@ make twonav_velo_defconfig
 make oldconfig
 make -j4
 make modules_install INSTALL_MOD_PATH=$KLIB && sync
-make modules_install INSTALL_MOD_PATH=/media/ebosch/trusty && sync
+make modules_install INSTALL_MOD_PATH=/media/$HOMEUSERFOLDER/trusty && sync
+
+echo "**** STEP 3 END BUILD KERNEL AS USUAL ****"
 
 #4.BUILD BACKPORTS
 
@@ -31,6 +59,8 @@ make defconfig-wifi
 make oldconfig
 make -j4
 make install
+
+echo "**** STEP 4 END BUILD BACKPORTS ****"
 
 #5.CHECK NEW KERNEL NAME
 
@@ -47,18 +77,26 @@ done
 
 echo "Current kernel compilation: $kernel_name"
 
+echo "**** STEP 5 END CHECK NEW KERNEL NAME ****"
+
 #6.INSTALL MODULES
 
-cp -r $KLIB/lib/modules/$kernel_name/updates /media/ebosch/trusty/lib/modules/$kernel_name/wireless_backports
-cp $KERNEL_SRC/arch/arm/boot/zImage /media/ebosch/BOOT/zImage
+cp -r $KLIB/lib/modules/$kernel_name/updates /media/$HOMEUSERFOLDER/trusty/lib/modules/$kernel_name/wireless_backports
+cp $KERNEL_SRC/arch/arm/boot/zImage /media/$HOMEUSERFOLDER/BOOT/zImage
 cd $KERNEL_SRC
-make modules_install INSTALL_MOD_PATH=/media/ebosch/trusty && sync
+make modules_install INSTALL_MOD_PATH=/media/$HOMEUSERFOLDER/trusty && sync
+
+echo "**** STEP 6 END INSTALL MODULES ****"
 
 #7.BUILD PACKAGE
 cd $KERNEL_SRC
 DEB_HOST_ARCH=armhf make-kpkg --revision=1.0.0Velo -j5 --rootcmd fakeroot --arch arm --cross-compile arm-linux-gnueabihf- --initrd --zImage linux_headers linux_image
 cp extras/zImage debian/linux-image-$kernel_name/etc/kernel/postinst.d/update_zImage
 cp extras/uInitrd debian/linux-image-$kernel_name/etc/kernel/postinst.d/update_uInitrd
-cp -r /home/ebosch/Velo_images/kernel_modules/lib/modules/$kernel_name/updates debian/linux-image-$kernel_name/lib/modules/$kernel_name/wireless_backports
-dpkg --build /home/ebosch/Kernels_IMASD/Clickarm_Kernel_3.8/debian/linux-image-$kernel_name ..
-dpkg --build /home/ebosch/Kernels_IMASD/Clickarm_Kernel_3.8/debian/linux-headers-$kernel_name ..
+cp -r /home/$HOMEUSERFOLDER/Velo_images/kernel_modules/lib/modules/$kernel_name/updates debian/linux-image-$kernel_name/lib/modules/$kernel_name/wireless_backports
+dpkg --build /home/$HOMEUSERFOLDER/Kernels_IMASD/Clickarm_Kernel_3.8/debian/linux-image-$kernel_name ..
+dpkg --build /home/$HOMEUSERFOLDER/Kernels_IMASD/Clickarm_Kernel_3.8/debian/linux-headers-$kernel_name ..
+
+echo "**** STEP 7 END ****"
+
+
