9f1fc6499fa561fc16cf503102ec3f7e408e280f
Eric Bosch <ebosch@twonav.com>
2018-04-19 17:08:51 +0200

    Adding config to mach-TwoNav (UNCONFIGURED)

--- arch/arm/mach-exynos/mach-TwoNav.c
+++ arch/arm/mach-exynos/mach-TwoNav.c
@@ -198,6 +198,210 @@ static struct usb3503_platform_data usb3503_pdata = {
 #endif
 /*END OF USB 3505 SMC CCOnfiguration*/
 
+/*CYTTSP5 Capacitive Touchscreen Configuration*/
+
+/* cyttsp */
+#include <linux/cyttsp5_core.h>
+#include <linux/cyttsp5_platform.h>
+
+#define CYTTSP5_USE_I2C
+
+#ifdef CYTTSP5_USE_I2C
+	#define CYTTSP5_I2C_TCH_ADR 0x24
+	#define CYTTSP5_LDR_TCH_ADR 0x24
+	#define CYTTSP5_I2C_IRQ_GPIO 38 /* TBD */
+	#define CYTTSP5_I2C_RST_GPIO 37 /* TBD */
+#endif
+
+#ifndef CONFIG_TOUCHSCREEN_CYPRESS_CYTTSP5_DEVICETREE_SUPPORT
+
+#define CYTTSP5_HID_DESC_REGISTER 1
+
+#define CY_VKEYS_X 720
+#define CY_VKEYS_Y 1280
+#define CY_MAXX 880
+#define CY_MAXY 1280
+#define CY_MINX 0
+#define CY_MINY 0
+
+#define CY_ABS_MIN_X CY_MINX
+#define CY_ABS_MIN_Y CY_MINY
+#define CY_ABS_MAX_X CY_MAXX
+#define CY_ABS_MAX_Y CY_MAXY
+#define CY_ABS_MIN_P 0
+#define CY_ABS_MAX_P 255
+#define CY_ABS_MIN_W 0
+#define CY_ABS_MAX_W 255
+#define CY_PROXIMITY_MIN_VAL	0
+#define CY_PROXIMITY_MAX_VAL	1
+
+#define CY_ABS_MIN_T 0
+
+#define CY_ABS_MAX_T 15
+
+/* Button to keycode conversion */
+static u16 cyttsp5_btn_keys[] = {
+	/* use this table to map buttons to keycodes (see input.h) */
+	KEY_HOMEPAGE,		/* 172 */ /* Previously was KEY_HOME (102) */
+				/* New Android versions use KEY_HOMEPAGE */
+	KEY_MENU,		/* 139 */
+	KEY_BACK,		/* 158 */
+	KEY_SEARCH,		/* 217 */
+	KEY_VOLUMEDOWN,		/* 114 */
+	KEY_VOLUMEUP,		/* 115 */
+	KEY_CAMERA,		/* 212 */
+	KEY_POWER		/* 116 */
+};
+
+static struct touch_settings cyttsp5_sett_btn_keys = {
+	.data = (uint8_t *)&cyttsp5_btn_keys[0],
+	.size = ARRAY_SIZE(cyttsp5_btn_keys),
+	.tag = 0,
+};
+
+static struct cyttsp5_core_platform_data _cyttsp5_core_platform_data = {
+	.irq_gpio = CYTTSP5_I2C_IRQ_GPIO,
+	.rst_gpio = CYTTSP5_I2C_RST_GPIO,
+	.hid_desc_register = CYTTSP5_HID_DESC_REGISTER,
+	.xres = cyttsp5_xres,
+	.init = cyttsp5_init,
+	.power = cyttsp5_power,
+	.detect = cyttsp5_detect,
+	.irq_stat = cyttsp5_irq_stat,
+	.sett = {
+		NULL,	/* Reserved */
+		NULL,	/* Command Registers */
+		NULL,	/* Touch Report */
+		NULL,	/* Parade Data Record */
+		NULL,	/* Test Record */
+		NULL,	/* Panel Configuration Record */
+		NULL,	/* &cyttsp5_sett_param_regs, */
+		NULL,	/* &cyttsp5_sett_param_size, */
+		NULL,	/* Reserved */
+		NULL,	/* Reserved */
+		NULL,	/* Operational Configuration Record */
+		NULL, /* &cyttsp5_sett_ddata, *//* Design Data Record */
+		NULL, /* &cyttsp5_sett_mdata, *//* Manufacturing Data Record */
+		NULL,	/* Config and Test Registers */
+		&cyttsp5_sett_btn_keys,	/* button-to-keycode table */
+	},
+	.flags = CY_CORE_FLAG_RESTORE_PARAMETERS,
+	.easy_wakeup_gesture = CY_CORE_EWG_NONE,
+};
+
+static const int16_t cyttsp5_abs[] = {
+	ABS_MT_POSITION_X, CY_ABS_MIN_X, CY_ABS_MAX_X, 0, 0,
+	ABS_MT_POSITION_Y, CY_ABS_MIN_Y, CY_ABS_MAX_Y, 0, 0,
+	ABS_MT_PRESSURE, CY_ABS_MIN_P, CY_ABS_MAX_P, 0, 0,
+	CY_IGNORE_VALUE, CY_ABS_MIN_W, CY_ABS_MAX_W, 0, 0,
+	ABS_MT_TRACKING_ID, CY_ABS_MIN_T, CY_ABS_MAX_T, 0, 0,
+	ABS_MT_TOUCH_MAJOR, 0, 255, 0, 0,
+	ABS_MT_TOUCH_MINOR, 0, 255, 0, 0,
+	ABS_MT_ORIENTATION, -127, 127, 0, 0,
+	ABS_MT_TOOL_TYPE, 0, MT_TOOL_MAX, 0, 0,
+	ABS_MT_DISTANCE, 0, 255, 0, 0,	/* Used with hover */
+};
+
+struct touch_framework cyttsp5_framework = {
+	.abs = (uint16_t *)&cyttsp5_abs[0],
+	.size = ARRAY_SIZE(cyttsp5_abs),
+	.enable_vkeys = 0,
+};
+
+static struct cyttsp5_mt_platform_data _cyttsp5_mt_platform_data = {
+	.frmwrk = &cyttsp5_framework,
+	.flags = CY_MT_FLAG_INV_X | CY_MT_FLAG_INV_Y,
+	.inp_dev_name = CYTTSP5_MT_NAME,
+	.vkeys_x = CY_VKEYS_X,
+	.vkeys_y = CY_VKEYS_Y,
+};
+
+static struct cyttsp5_btn_platform_data _cyttsp5_btn_platform_data = {
+	.inp_dev_name = CYTTSP5_BTN_NAME,
+};
+
+static const int16_t cyttsp5_prox_abs[] = {
+	ABS_DISTANCE, CY_PROXIMITY_MIN_VAL, CY_PROXIMITY_MAX_VAL, 0, 0,
+};
+
+struct touch_framework cyttsp5_prox_framework = {
+	.abs = (uint16_t *)&cyttsp5_prox_abs[0],
+	.size = ARRAY_SIZE(cyttsp5_prox_abs),
+};
+
+static struct cyttsp5_proximity_platform_data
+		_cyttsp5_proximity_platform_data = {
+	.frmwrk = &cyttsp5_prox_framework,
+	.inp_dev_name = CYTTSP5_PROXIMITY_NAME,
+};
+
+static struct cyttsp5_platform_data _cyttsp5_platform_data = {
+	.core_pdata = &_cyttsp5_core_platform_data,
+	.mt_pdata = &_cyttsp5_mt_platform_data,
+	.loader_pdata = &_cyttsp5_loader_platform_data,
+	.btn_pdata = &_cyttsp5_btn_platform_data,
+	.prox_pdata = &_cyttsp5_proximity_platform_data,
+};
+
+static ssize_t cyttsp5_virtualkeys_show(struct kobject *kobj,
+		struct kobj_attribute *attr, char *buf)
+{
+	return sprintf(buf,
+		__stringify(EV_KEY) ":"
+		__stringify(KEY_BACK) ":1360:90:160:180:"
+		__stringify(EV_KEY) ":"
+		__stringify(KEY_MENU) ":1360:270:160:180:"
+		__stringify(EV_KEY) ":"
+		__stringify(KEY_HOMEPAGE) ":1360:450:160:180:"
+		__stringify(EV_KEY) ":"
+		__stringify(KEY_SEARCH) ":1360:630:160:180\n");
+}
+
+static struct kobj_attribute cyttsp5_virtualkeys_attr = {
+	.attr = {
+		.name = "virtualkeys.cyttsp5_mt",
+		.mode = S_IRUGO,
+	},
+	.show = &cyttsp5_virtualkeys_show,
+};
+
+static struct attribute *cyttsp5_properties_attrs[] = {
+	&cyttsp5_virtualkeys_attr.attr,
+	NULL
+};
+
+static struct attribute_group cyttsp5_properties_attr_group = {
+	.attrs = cyttsp5_properties_attrs,
+};
+#endif /* !CONFIG_TOUCHSCREEN_CYPRESS_CYTTSP5_DEVICETREE_SUPPORT */
+
+static void __init twonav_cyttsp5_init(void)
+{
+	/* Initialize muxes for GPIO pins */
+#ifdef CYTTSP5_USE_I2C
+	//omap_mux_init_gpio(CYTTSP5_I2C_RST_GPIO, OMAP_PIN_OUTPUT);
+	//omap_mux_init_gpio(CYTTSP5_I2C_IRQ_GPIO, OMAP_PIN_INPUT_PULLUP);
+#endif
+#ifdef CYTTSP5_USE_SPI
+	//omap_mux_init_gpio(CYTTSP5_SPI_RST_GPIO, OMAP_PIN_OUTPUT);
+	//omap_mux_init_gpio(CYTTSP5_SPI_IRQ_GPIO, OMAP_PIN_INPUT_PULLUP);
+#endif
+
+#ifndef CONFIG_TOUCHSCREEN_CYPRESS_CYTTSP5_DEVICETREE_SUPPORT
+	struct kobject *properties_kobj;
+	int ret = 0;
+
+	properties_kobj = kobject_create_and_add("board_properties", NULL);
+	if (properties_kobj)
+		ret = sysfs_create_group(properties_kobj,
+				&cyttsp5_properties_attr_group);
+	if (!properties_kobj || ret)
+		pr_err("%s: failed to create board_properties\n", __func__);
+#endif
+}
+
+/*End of CYTTSP5 Capacitive Touchscreen Configuration*/
+
 /*touchscreen config tsc2007 XE_INT22*/
 #if defined(CONFIG_TOUCHSCREEN_TSC2007) || defined(CONFIG_TOUCHSCREEN_TSC2007_MODULE)
 #include <linux/i2c/tsc2007.h>
@@ -399,6 +603,16 @@ static struct i2c_board_info twonav_i2c_devs1[] __initdata = {
 		.platform_data  = &max44005_driver, /*CONFIG PDATA*/
 
 	},
+#endif
+
+#ifndef CONFIG_TOUCHSCREEN_CYPRESS_CYTTSP5_DEVICETREE_SUPPORT
+#ifdef CYTTSP5_USE_I2C
+	{
+		I2C_BOARD_INFO(CYTTSP5_I2C_NAME, CYTTSP5_I2C_TCH_ADR),
+		.irq =  CYTTSP5_I2C_IRQ_GPIO,
+		.platform_data = &_cyttsp5_platform_data,
+	},
+#endif
 #endif
 };
 /*END OF Devices Conected on I2C BUS 1 LISTED ABOVE*/
@@ -1287,6 +1501,8 @@ static void __init twonav_machine_init(void)
 	exynos4_fimd0_gpio_setup_24bpp();
 #endif
 	init_button_irqs();
+
+	twonav_cyttsp5_init();
 
 	platform_add_devices(twonav_devices, ARRAY_SIZE(twonav_devices));
 
